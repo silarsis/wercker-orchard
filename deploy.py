746331a04f613d2fc1f8cc8d103196c84d2e9964
Kevin Littlejohn <kevin@littlejohn.id.au>
2014-06-27 23:46:52 +1000

    attempt to import results to orchard

--- deploy.py
+++ deploy.py
@@ -4,3 +4,4 @@ import orchard
 
 client = orchard.with_token(ENV['ORCHARD_TOKEN'])
 docker = client.hosts[ENV.get('ORCHARD_HOST_ID', 'default')].docker()
+docker.import_image(open("%s/arbitrage.tar" % ENV['WERCKER_OUTPUT_DIR']), tag='arbitrage')
