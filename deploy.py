0ce9c4395db6ce739efa53a459227b5c6e663b0e
Kevin Littlejohn <kevin@littlejohn.id.au>
2014-06-28 22:56:53 +1000

    forgot to remove a closing bracket

--- deploy.py
+++ deploy.py
@@ -6,5 +6,5 @@ import orchard
 client = orchard.with_token(os.environ['WERCKER_ORCHARDUP_DEPLOY_TOKEN'])
 docker = client.hosts[os.environ.get('WERCKER_ORCHARDUP_HOST_ID', 'default')].docker()
 docker.import_image(
-  "%s/%s" % (os.environ['WERCKER_ROOT'], os.environ['WERCKER_ORCHARDUP_DEPLOY_EXPORT_FILENAME'])),
+  "%s/%s" % (os.environ['WERCKER_ROOT'], os.environ['WERCKER_ORCHARDUP_DEPLOY_EXPORT_FILENAME']),
   tag=os.environ['WERCKER_ORCHARDUP_DEPLOY_IMAGE_TAG'])
