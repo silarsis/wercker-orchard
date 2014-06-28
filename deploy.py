569fbef68dd5cf377fe91e4438fce9325b7054be
Kevin Littlejohn <kevin@littlejohn.id.au>
2014-06-28 21:34:47 +1000

    patching - try as it's provided

--- deploy.py
+++ deploy.py
@@ -6,5 +6,5 @@ import orchard
 client = orchard.with_token(os.environ['WERCKER_ORCHARDUP_DEPLOY_TOKEN'])
 docker = client.hosts[os.environ.get('WERCKER_ORCHARDUP_HOST_ID', 'default')].docker()
 docker.import_image(
-  open("%s/%s" % (os.environ['WERCKER_OUTPUT_DIR'], os.environ['WERCKER_ORCHARDUP_DEPLOY_EXPORT_FILENAME'])),
+  open(os.environ['WERCKER_ORCHARDUP_DEPLOY_EXPORT_FILENAME']),
   tag=os.environ['WERCKER_ORCHARDUP_DEPLOY_IMAGE_TAG'])
