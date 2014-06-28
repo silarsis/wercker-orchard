169b5a87b1c44ef9680de8b08605469c8a466c73
Kevin Littlejohn <kevin@littlejohn.id.au>
2014-06-28 16:55:45 +1000

    oops, missing bracket

--- deploy.py
+++ deploy.py
@@ -4,4 +4,4 @@ import orchard
 
 client = orchard.with_token(ENV['WERCKER_ORCHARDUP_DEPLOY_TOKEN'])
 docker = client.hosts[ENV.get('WERCKER_ORCHARDUP_HOST_ID', 'default')].docker()
-docker.import_image(open("%s/%s" % (ENV['WERCKER_OUTPUT_DIR'], ENV['WERCKER_ORCHARDUP_DEPLOY_EXPORT_FILENAME']), tag=ENV['WERCKER_ORCHARDUP_DEPLOY_IMAGE_TAG'])
+docker.import_image(open("%s/%s" % (ENV['WERCKER_OUTPUT_DIR'], ENV['WERCKER_ORCHARDUP_DEPLOY_EXPORT_FILENAME'])), tag=ENV['WERCKER_ORCHARDUP_DEPLOY_IMAGE_TAG'])
