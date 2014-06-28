ac0f71bab227262d68d44e6c8f05d2227170e806
Kevin Littlejohn <kevin@littlejohn.id.au>
2014-06-28 22:37:46 +1000

    this time for sure

--- run.sh
+++ run.sh
@@ -15,8 +15,6 @@ if [ ! -n "$WERCKER_ORCHARDUP_DEPLOY_IMAGE_TAG" ]; then
   fatal 'Please specify token property'
 fi
 
-pwd
-find / -iname $WERCKER_ORCHARDUP_DEPLOY_EXPORT_FILENAME
 sudo apt-get install -y python-pip
 cd ${WERCKER_STEP_ROOT}
 sudo pip install orchard
