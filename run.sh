749ccf7d25729b9df0c3c3fef028aadae18b3bab
Kevin Littlejohn <kevin@littlejohn.id.au>
2014-07-03 06:50:37 +1000

    follow github redirects

--- run.sh
+++ run.sh
@@ -10,7 +10,7 @@ if [ ! -n "$WERCKER_ORCHARDUP_DEPLOY_EXPORT_FILENAME" ]; then
   fatal 'Please specify token property'
 fi
 
-curl https://github.com/orchardup/go-orchard/releases/download/2.0.7/linux > ./orchard
+curl -L -o ./orchard https://github.com/orchardup/go-orchard/releases/download/2.0.7/linux
 chmod +x ./orchard
 ORCHARD_API_TOKEN=${WERCKER_ORCHARDUP_DEPLOY_TOKEN} ./orchard docker load ${WERCKER_ROOT}/${WERCKER_ORCHARDUP_DEPLOY_EXPORT_FILENAME}
 success "Successfully deployed to Orchard ready for restart"
